0e6bedba40c99ceebb196345bcc10147c3078829
Daniel Souza <eu@danielsouza.dev.br>
2021-11-10 05:09:44 -0300

    .

--- single.php
+++ single.php
@@ -62,42 +62,70 @@ $theme = get_bloginfo("template_url") . '/dist';
       </div>
     </div>
   </section>
+  <?php
+  if (get_previous_post() || get_next_post()) {
+  ?>
+    <section>
+      <div class="container pt-4 pb-ilaese-128">
+        <div class="row justify-content-center">
+          <div class="col-md-11 col-lg-10">
+            <div class="row gapy-3">
+              <?php
+              if (get_previous_post()) {
+              ?>
+                <div class="col-10 col-md-6">
+                  <div class="d-flex flex-column">
+                    <a href="<?= get_the_permalink(get_previous_post()); ?>" class="fw-bold link-ilaese-second text-decoration-none mb-3">
+                      <i class="fas fa-chevron-left me-1"></i> Anterior
+                    </a>
+                    <h2 class="fw-bold fz-20 text-ilaese-black ff-playfair m-0">
+                      <?= get_the_title(get_previous_post()) ?>
+                    </h2>
+                  </div>
+                </div>
+              <?php
+              }
+              if (get_next_post()) {
+              ?>
+                <div class="col-10 col-md-6 ms-auto">
+                  <div class="d-flex flex-column text-end">
+                    <a href="<?= get_the_permalink(get_next_post()); ?>" class="fw-bold link-ilaese-second text-decoration-none mb-3 ms-auto">
+                      Próximo <i class="fas fa-chevron-right me-1"></i>
+                    </a>
+                    <h2 class="fw-bold fz-20 text-ilaese-black ff-playfair m-0">
+                      <?= get_the_title(get_next_post()) ?>
+                    </h2>
+                  </div>
+                </div>
+              <?php
+              }
+              ?>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  <?php
+  }
+  ?>
+
   <section>
-    <div class="container pt-4 pb-ilaese-128">
+    <div class="container pt-5 pb-ilaese-128">
       <div class="row justify-content-center">
         <div class="col-md-11 col-lg-10">
-          <div class="row gapy-3">
-            <?php
-            if (get_previous_post()) {
-            ?>
-              <div class="col-10 col-md-6">
-                <div class="d-flex flex-column">
-                  <a href="<?= get_the_permalink(get_previous_post()); ?>" class="fw-bold link-ilaese-second text-decoration-none mb-3">
-                    <i class="fas fa-chevron-left me-1"></i> Anterior
-                  </a>
-                  <h2 class="fw-bold fz-20 text-ilaese-black ff-playfair m-0">
-                    <?= get_the_title(get_previous_post()) ?>
-                  </h2>
-                </div>
-              </div>
-            <?php
-            }
-            if (get_next_post()) {
-            ?>
-              <div class="col-10 col-md-6 ms-auto">
-                <div class="d-flex flex-column text-end">
-                  <a href="<?= get_the_permalink(get_next_post()); ?>" class="fw-bold link-ilaese-second text-decoration-none mb-3 ms-auto">
-                    Próximo <i class="fas fa-chevron-right me-1"></i>
-                  </a>
-                  <h2 class="fw-bold fz-20 text-ilaese-black ff-playfair m-0">
-                    <?= get_the_title(get_next_post()) ?>
-                  </h2>
-                </div>
-              </div>
-            <?php
-            }
-            ?>
-          </div>
+          <?php comment_form(array(
+            'fields' => array(
+              'author' => '<div class="col-12 col-md-6 order-0"><div class="d-flex flex-column gapy-3"><input class="form-control form-control-lg input-ilaese-gray-fifth" type="text" name="author" placeholder="Nome" /> <input class="form-control form-control-lg input-ilaese-gray-fifth" type="email" name="email" placeholder="E-mail" /> </div> </div>',
+              'url' => '',
+              'cookies' => '',
+            ),
+            'comment_field' => '<div class="col-12 col-md-' . (is_user_logged_in() ? '12' : '6') . ' order-1"><textarea class="form-control form-control-lg input-ilaese-gray-fifth h-100" name="comment" type="text" placeholder="Mensagem"></textarea></div>',
+            'comment_notes_before' => '',
+            'label_submit' => 'PUBLICAR COMENTÁRIO',
+            'submit_field' => '<div class="ms-auto mt-4 order-5">%1$s %2$s</div>',
+            'logged_in_as' => '',
+            'title_reply' => 'Deixe uma resposta',
+          )); ?>
         </div>
       </div>
     </div>
